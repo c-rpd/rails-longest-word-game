d1d6e56dc45d80fa79adce404355042f46602fc9
Corentin Ripaud <corentin.ripaud@gmail.com>
2024-02-19 16:44:20 +0100

    longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+  def new
+    @letters = []
+    # 10.times
+    while @letters.length <= 10
+      @letters << ('a'..'z').to_a.sample
+    end
+  end
+
+  def score
+  end
 end
